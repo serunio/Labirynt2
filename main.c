a1931cfba2d12eb922a9031dae7cf7aefcb941f7
serunio <jak.haraszkiewicz@gmail.com>
2024-04-09 23:32:01 +0200

    komentarze

--- main.c
+++ main.c
@@ -15,14 +15,15 @@ int main()
     char** matrix = malloc(xLabiryntu*sizeof(char*));
     for(int i = 0; i < xLabiryntu; i++)
         //x - informacja o tym w ktora strone mozna przejsc:
-        //prawo, lewo, góra, dół
+        //góra, prawo, lewo, dół
         matrix[i] = malloc(4*sizeof(char));
 
     int aktywnaKomorka[2] = {0,0}; //położenie y,x
 
     PobierzWiersz(in, xLabiryntu, aktywnaKomorka[0], matrix);
 
-    //
+    //reszta z dzielenia przez 4 ze zwrot to kierunek w którym patrzy program
+    //0 - góra, 1 - prawo, 2 - dół, 3 - lewo
     int zwrot = 4001;
 
     DFS(in, xLabiryntu, aktywnaKomorka, zwrot, matrix);
